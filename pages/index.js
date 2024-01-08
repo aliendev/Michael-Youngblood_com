a2b43ecef6b750c90c5c657580903979f7be2bcd
Michael Youngblood <michael.youngblood@alien.studio>
2024-01-08 08:29:24 -0600

    clean out the homepage

--- pages/index.js
+++ pages/index.js
@@ -12,23 +12,13 @@ export default function Index({ preview, allPosts }) {
   const morePosts = allPosts.slice(1)
   return (
     <>
-      <Layout preview={preview}>
+      <Layout>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>{`Michael Youngblood, Software Engineer Manager, Blog and CV`}</title>
         </Head>
+        <Intro />
         <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          
         </Container>
       </Layout>
     </>
