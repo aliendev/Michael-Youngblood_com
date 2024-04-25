9c3b0dfb16aa165c10baeaf0d2dff174702dbaab
Michael Youngblood <michael.youngblood@alien.studio>
2024-04-25 07:30:07 -0500

    made footer look a smidge better

--- components/footer.js
+++ components/footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
       <Container>
 
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <div class="col-md-4 d-flex align-items-center">
+          <div class="d-flex align-items-center">
             <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
               {/* TODO: Add SVG logo for me. */}
               {/* <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg> */}
