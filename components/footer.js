7daaa8fe4beb068f8a0a3d15912d84221267a46a
Michael Youngblood <michael.youngblood@gmail.com>
2025-02-22 10:49:52 -0600

    added link fixing #4 and moved icons fixing #5

--- components/footer.js
+++ components/footer.js
@@ -16,13 +16,14 @@ export default function Footer() {
               {/* TODO: Add SVG logo for me. */}
               {/* <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg> */}
             {/*</a> */}
-            <span class="mb-3 mb-md-0 text-muted">Copyright &copy; 1982 - {currentYear} <a href="/">Michael Youngblood</a>. All rights reserved.</span>
+            <span class="mb-4 mb-md-0 text-muted">Copyright &copy; 1982 - {currentYear} <a href="/">Michael Youngblood</a>. All rights reserved.</span>
           </div>
 
-          <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <ul class="nav col-md-3 justify-content-end list-unstyled d-flex">
             <li class="ms-3"><Link className="text-muted" href="https://aliendev.com">AlienDev.com</Link></li>
+            <li class="ms-3"><Link className="text-muted" href="https://github.com/aliendev/Michael-Youngblood_com"><Github title='Code Repo for Michael-Youngblood.com' className='align-text-top' /> Code Repo</Link></li>
           </ul>
-          <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <ul class="nav col-md-2 justify-content-end list-unstyled d-flex">
             <li class="ms-3"><Link className="text-muted" href="https://x.com/aliendev2"><TwitterX title='AlienDev on Twitter'/></Link></li>
             <li class="ms-3"><Link className="text-muted" href="https://www.twitch.tv/aliendev2"><Twitch title='AlienDev on Twitch' /></Link></li>
             <li class="ms-3"><Link className="text-muted" href="https://github.com/aliendev"><Github title='Michael Youngblood on Github' /></Link></li>
