cb98ca7690a0fd6e3328823de9c9db6a8a452b98
Michael Youngblood <michael.youngblood@gmail.com>
2025-02-22 09:20:58 -0600

    update footer links

--- components/footer.js
+++ components/footer.js
@@ -10,25 +10,21 @@ export default function Footer() {
 
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <div class="d-flex align-items-center">
-            <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+            {/* <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"> */}
               {/* TODO: Add SVG logo for me. */}
               {/* <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg> */}
-            </a>
-            <span class="mb-3 mb-md-0 text-muted">Copyright &copy; 1982 - {currentYear} Michael Youngblood. All rights reserved.</span>
+            {/*</a> */}
+            <span class="mb-3 mb-md-0 text-muted">Copyright &copy; 1982 - {currentYear} <a href="/">Michael Youngblood</a>. All rights reserved.</span>
           </div>
 
           <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
             <li class="ms-3"><a class="text-muted" href="https://aliendev.com">AlienDev.com</a></li>
-            <li class="ms-3"><a class="text-muted" href="https://alien.studio">Alien.Studio</a></li>
           </ul>
-          
-          {/* TODO: Add social media links. 
           <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+            <li class="ms-3"><a class="text-muted" href="https://x.com/aliendev2"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+            <li class="ms-3"><a class="text-muted" href="https://bsky.app/profile/aliendev.bsky.social"><svg class="bi" width="24" height="24"><use xlink:href="#bluesky"></use></svg></a></li>
+            <li class="ms-3"><a class="text-muted" href="https://github.com/aliendev"><svg class="bi" width="24" height="24"><use xlink:href="#github"></use></svg></a></li>
           </ul>
-          */}
         </footer>
 
       </Container>
