902919fe03b8db42e551a150a51e5dc78208f87a
Michael Youngblood <michael.youngblood@gmail.com>
2025-06-03 14:09:16 -0500

    updating note.

--- components/PageFeaturedProduct/PageFeaturedProduct.jsx
+++ components/PageFeaturedProduct/PageFeaturedProduct.jsx
@@ -14,7 +14,7 @@ export default function PageFeaturedProduct() {
       <div className="col-lg-6">
         <h1 className="display-5 fw-bold lh-1 mb-3">Michael Youngblood</h1>
         <p className="lead mb-6">Software Engineer Manager (Web)</p>
-        <p><strong>Note:</strong> I use this site to help test other projects I may be working on.</p>
+        <p><strong>Note:</strong> I use this site to help test other projects I may be working on, it may or may not have weird features all over the place.</p>
         <div className="d-grid gap-2 d-md-flex justify-content-md-start">
           {/*
                     <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
